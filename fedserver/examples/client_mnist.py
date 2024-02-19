a2577825fcc3291f47b7104893740379bbd15e20
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2024-02-19 13:12:42 +0100

    Update examples

---
+++ fedserver/examples/client_mnist.py
@@ -0,0 +1,85 @@
+import pathlib
+import sys
+
+import flwr as fl
+from keras.datasets import mnist
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
+import ai4flwr.auth.bearer
+
+# Check if Token is provided in cmdline
+#if len(sys.argv) != 2:
+#    print("Error: No token provided!!!")
+#    print(f"Usage: {sys.argv[0]} <token>")
+#    sys.exit(1)
+
+token = "1234"
+
+# Load and process MNIST data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = np.reshape(X_train, (60000, 28, 28, 1))
+X_train = X_train.astype('float32') / 255
+X_test = np.reshape(X_test, (10000, 28, 28, 1))
+X_test = X_test.astype('float32') / 255
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Create a new train/test set for client 1:
+x_train = X_train[:len(X_train)//3]
+y_train = y_train[:len(X_train)//3]
+
+x_test = X_test[:len(X_test)//3]
+y_test = y_test[:len(X_test)//3]
+
+# Model to be trained:
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add((MaxPooling2D((2, 2))))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add((MaxPooling2D((2, 2))))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+model.summary()
+
+# Flower and auth configuration
+
+
+# Flower client
+class Client(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=5, batch_size=32)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+
+auth_plugin = ai4flwr.auth.bearer.BearerTokenAuthPlugin(token)
+
+
+fl.client.start_client(
+    server_address="localhost:5000",
+    root_certificates=pathlib.Path(
+        "fedserver", "examples", ".cache", "certificates", "ca.crt"
+    ).read_bytes(),
+    client=Client(),
+    call_credentials=auth_plugin.call_credentials(),
+)
---
+++ fedserver/examples/server.py
@@ -0,0 +1,78 @@
+import ast
+import pathlib
+
+import flwr as fl
+import ai4flwr.auth.bearer
+
+FEDERATED_ROUNDS = 2
+FEDERATED_METRIC = "accuracy"
+FEDERATED_MIN_CLIENTS = 2
+FEDERATED_STRATEGY = "Federated Averaging"
+FEDERATED_TOKENS = "1234"
+
+# Weighted average of the metric:
+def wavg_metric(metrics):
+    global FEDERATED_METRIC
+    list_metrics = []
+    try:
+        list_metrics = ast.literal_eval(FEDERATED_METRIC)
+    except ValueError:
+        print("Only one metric has been entered.")
+    if len(list_metrics) == 0:
+        n = sum([i for i, _ in metrics])
+        wavg_metric = sum([i * metric[FEDERATED_METRIC] / n for i, metric in metrics])
+        return {FEDERATED_METRIC: wavg_metric}
+    else:
+        n = sum([i for i, _ in metrics])
+        dict_metrics = {}
+        for fed_metric in list_metrics:
+            wavg_metric = sum([i * metric[fed_metric] / n for i, metric in metrics])
+            dict_metrics[fed_metric] = wavg_metric
+        return dict_metrics
+
+
+if FEDERATED_STRATEGY == "Federated Averaging" or FEDERATED_STRATEGY is None:
+    strategy = fl.server.strategy.FedAvg(
+        min_available_clients=FEDERATED_MIN_CLIENTS,
+        min_fit_clients=FEDERATED_MIN_CLIENTS,
+        evaluate_metrics_aggregation_fn=wavg_metric,
+    )
+elif FEDERATED_STRATEGY == "FedProx Strategy":
+    strategy = fl.server.strategy.FedProx(
+        min_available_clients=FEDERATED_MIN_CLIENTS,
+        min_fit_clients=FEDERATED_MIN_CLIENTS,
+        evaluate_metrics_aggregation_fn=wavg_metric,
+    )
+elif FEDERATED_STRATEGY == "Federated Optim Strategy":
+    strategy = fl.server.strategy.FedOpt(
+        min_available_clients=FEDERATED_MIN_CLIENTS,
+        min_fit_clients=FEDERATED_MIN_CLIENTS,
+        evaluate_metrics_aggregation_fn=wavg_metric,
+    )
+elif FEDERATED_STRATEGY == "Federated Optimization with Adam":
+    strategy = fl.server.strategy.FedAdam(
+        min_available_clients=FEDERATED_MIN_CLIENTS,
+        min_fit_clients=FEDERATED_MIN_CLIENTS,
+        evaluate_metrics_aggregation_fn=wavg_metric,
+    )
+elif FEDERATED_STRATEGY == "Adaptive Federated Optimization using Yogi":
+    strategy = fl.server.strategy.FedYogi(
+        min_available_clients=FEDERATED_MIN_CLIENTS,
+        min_fit_clients=FEDERATED_MIN_CLIENTS,
+        evaluate_metrics_aggregation_fn=wavg_metric,
+    )
+
+# Include token interceptor
+token_interceptor = ai4flwr.auth.bearer.BearerTokenInterceptor(FEDERATED_TOKENS)
+# Flower server:
+fl.server.start_server(
+    server_address="0.0.0.0:5000",
+    config=fl.server.ServerConfig(num_rounds=FEDERATED_ROUNDS),
+    strategy=strategy,
+    certificates=(
+        pathlib.Path("fedserver", "examples", ".cache", "certificates", "ca.crt").read_bytes(),
+        pathlib.Path("fedserver", "examples", ".cache", "certificates", "server.pem").read_bytes(),
+        pathlib.Path("fedserver", "examples", ".cache", "certificates", "server.key").read_bytes(),
+    ),
+    interceptors=[token_interceptor]
+)
